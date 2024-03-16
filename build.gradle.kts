756e82cf60c4b88d066507b7719719662fe5c21d
Volodymyr Cholak <vcholak@users.noreply.github.com>
2024-03-16 12:12:25 -0600

    Gradle Kotlin.

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
+val prometeus_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+    id("io.ktor.plugin") version "2.3.9"
+}
+
+group = "org.ruthenia.itc"
+version = "0.0.1"
+
+application {
+    mainClass.set("org.ruthenia.itc.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-gson-jvm")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
+
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-client-core-jvm")
+    implementation("io.ktor:ktor-client-apache-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
